f787e0f4403a86f8d5c0b42f85b98929c3dea33c
Ramil Ibragimov <raul93.18@gmail.com>
2017-02-15 12:43:12 +0300

    LeftMenuTableViewControllerTests
    Добавлены тесты для контроллера меню.
    Обозначен провайдер данных.
    Обозначен делегат таблицы меню.

---
+++ XO/Controller/LeftMenuDataProvider.swift
@@ -0,0 +1,13 @@
+//
+//  LeftMenuDataProvider.swift
+//  XO
+//
+//  Created by Рамиль Ибрагимов on 15.02.17.
+//  Copyright © 2017 Рамиль Ибрагимов. All rights reserved.
+//
+
+import UIKit
+
+class LeftMenuDataProvider: NSObject, UITableViewDelegate {
+
+}
