6fbd9911023bc5431ea823d8e67cd03027e812f9
Ramil Ibragimov <raul93.18@gmail.com>
2017-02-15 15:05:36 +0300

    Animation OMarkView
    Анимация метки O

--- XO/OMarkView.swift
+++ XO/OMarkView.swift
@@ -28,24 +28,57 @@ class OMarkView: UIView {
     
     let lineWidth: CGFloat = 5
     
+    let durationOfAnimation: TimeInterval = 0.3
+    
+    var circleLayer: CAShapeLayer!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor(red: 100, green: 0, blue: 0, alpha: 0)
+        self.backgroundColor = UIColor.clear
+        
+        // легкий способ создать CGPath
+        let circlePath = UIBezierPath(arcCenter: centerOfCircle, radius: radiusOfCircle, startAngle: CGFloat(3*M_PI_2), endAngle: CGFloat(3*M_PI_2 - 2*M_PI), clockwise: false)
+        
+        // настраиваем circleLayer
+        circleLayer = CAShapeLayer()
+        circleLayer.path = circlePath.cgPath
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.strokeColor = UIColor.black.cgColor
+        circleLayer.lineWidth = lineWidth
+        
+        // чтобы при инициализации не произошло рисование - указываем параметр strokeEnd = 0.0
+        circleLayer.strokeEnd = 0.0
+        
+        // добавляем слой круга к слоям отображения
+        layer.addSublayer(circleLayer)
+        
+        // запускаем анимацию при инициализации
+        animateCurcle()
+    }
+
+    func animateCurcle() {
+        // мы хотим анимировать параметр strokeEnd слоя круга
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        // устанавливаем длительность анимации
+        animation.duration = durationOfAnimation
+        
+        // анимировать будем от 0 (нет круга) до 1 (полный круг)
+        animation.fromValue = 0
+        animation.toValue = 1
+        
+        // временная функция анимации
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        
+        // устанавливаем свойство strokeEnd для слоя круга
+        // то значение в котором должна закончится анимация
+        circleLayer.strokeEnd = 1.0
+        
+        // выполняем анимацию
+        circleLayer.add(animation, forKey: "animateCircle")
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func draw(_ rect: CGRect) {
-        drawCircle()
-    }
-    
-    func drawCircle(){
-        let circlePath = UIBezierPath(arcCenter: self.centerOfCircle, radius: self.radiusOfCircle, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: false)
-        
-        circlePath.lineWidth = lineWidth
-        
-        circlePath.stroke()
     }
 }
